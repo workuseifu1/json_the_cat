89f1240bae8ca74ea5a0a034c1637bb8b5259958
workuseifu1 <workuseifu1@gmail.com>
2023-05-25 17:22:58 -0600

    complete testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,20 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-const breedDataFromHttp = function(breedName, callback) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {    
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
     const data = JSON.parse(body);
-    return callback(false, data);
-  })
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      return callback(null, breed.description);
+    }
+
+  });
 }
-const printCatBreed = (breedName, (error, result) => {
-  if (error) {
-    console.log('Error fetch details:', error);
-  } else if (result[0] === undefined) {
-    console.log("Not cat breed");
-  } else {
-    console.log(result[0].description);
-  }
-});
-breedDataFromHttp(breedName, printCatBreed);
+
+module.exports = { fetchBreedDescription };
